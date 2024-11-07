732c75ef368bb6738b2bb4b40dd16c8f8b81f694
Muhammad Rafi Munggaran <ethanjeger123@gmail.com>
2024-11-07 12:42:11 +0700

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Test from "./Test";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
-      <section id="Services">Parallax</section>
-      <section>Services</section>
-      <section id="Portfolio">Parallax</section>
-      <section>Portofolio1</section>
-      <section>Portofolio2</section>
-      <section>Portofolio3</section>
-      <section id="Contact">Contact</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,84 @@
 import React from "react";
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -50,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 1,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "miror",
+      duration: 20,
+    },
+  },
+};
 
 const Hero = () => {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>RAFI MUNGGARAN</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web and Mobile Developer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt=""
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
+        <motion.div
+          className="slidingTextContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate"
+        >
+          Javascript PHP Python Dart
+        </motion.div>
+      </div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
